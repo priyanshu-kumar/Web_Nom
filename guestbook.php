ca43458131b8a4eb72dfe2ad72fe6a6f3e754e4e
priyanshu-kumar <priyanshukumar.0309@gmail.com>
2017-04-05 19:06:45 +0530

    Update guestbook.php

--- guestbook.php
+++ guestbook.php
@@ -2,19 +2,30 @@
 <!doctype html>
 <html>
 <head>
-	
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script>
+  $( function() {
+    $( "#date" ).datepicker();
+  } );
+  </script>
 </head>
 <body>
+
+	
 
 	<h1>PHP Register Entrybook</h1>
 	<a href="guestbook.php" >Home</a>
 	<a href="view_guestbook.php">View Entrybook</a><br><br>
 	<form method="post" action="update_guestbook.php">
-<!-- 		# server:localhost
+	    <!-- # server:localhost
 		# username:root
 		# password:NUL
 		# Database:web_nom
 		# table:entry -->
+
 	<table>
 		<tr>
 		<td>Name: </td>
@@ -26,13 +37,13 @@
 	    </tr>
 	    <tr>
 	    	<td>Date of birth: </td>
-	    	<td><input type="Date"  name="date_of_birth" /></td>
+	    	<td><input type="text" id='date' name="date_of_birth" /></td>
 	    </tr>
 	    <tr>
 	    	<td></td>
 	    <td><input type="submit" value="Submit Entry " name="submit"></td>
 	    </tr>
-
+<script type="text/javascript" src="jquery.js"></script>
 	</table>
 		
 	</form>
